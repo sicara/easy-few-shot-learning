184467b9e6dd6108c21fc3b8f1520475847ba35d
etienne <etienne.bennequin@gmail.com>
2021-03-12 16:21:22 +0100

    Mock cuda() in tests (was breaking CircleCI)

--- easyfsl/tests/methods/abstract_meta_learner_test.py
+++ easyfsl/tests/methods/abstract_meta_learner_test.py
@@ -45,18 +45,19 @@ class TestAMLEvaluateOnOneTask:
         expected_correct,
         expected_total,
     ):
-        with patch("easyfsl.methods.AbstractMetaLearner.forward") as mock_forward:
-            mock_forward.return_value = torch.tensor(5 * [[0.25, 0.75]]).cuda()
-            model = AbstractMetaLearner(resnet18())
-            assert (
-                model.evaluate_on_one_task(
-                    support_images,
-                    support_labels,
-                    query_images,
-                    query_labels,
+        with patch("torch.Tensor.cuda", new=torch.Tensor.cpu):
+            with patch("easyfsl.methods.AbstractMetaLearner.forward") as mock_forward:
+                mock_forward.return_value = torch.tensor(5 * [[0.25, 0.75]]).cuda()
+                model = AbstractMetaLearner(resnet18())
+                assert (
+                    model.evaluate_on_one_task(
+                        support_images,
+                        support_labels,
+                        query_images,
+                        query_labels,
+                    )
+                    == (expected_correct, expected_total)
                 )
-                == (expected_correct, expected_total)
-            )
 
 
 # pylint: enable=not-callable
