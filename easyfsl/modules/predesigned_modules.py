326bb6721fa6b38e0f9ae4fb3d545ec03b2d76f2
ebennequin <etienne.bennequin@gmail.com>
2022-03-08 16:46:56 +0100

    Reformat predesigned_modules

--- easyfsl/modules/predesigned_modules.py
+++ easyfsl/modules/predesigned_modules.py
@@ -15,41 +15,34 @@ __all__ = [
 
 def resnet10(**kwargs):
     """Constructs a ResNet-10 model."""
-    model = ResNet(BasicBlock, [1, 1, 1, 1], **kwargs)
-    return model
+    return ResNet(BasicBlock, [1, 1, 1, 1], **kwargs)
 
 
 def resnet12(**kwargs):
     """Constructs a ResNet-12 model."""
-    model = ResNet(BasicBlock, [1, 1, 2, 1], planes=[64, 160, 320, 640], **kwargs)
-    return model
+    return ResNet(BasicBlock, [1, 1, 2, 1], planes=[64, 160, 320, 640], **kwargs)
 
 
 def resnet18(**kwargs):
     """Constructs a ResNet-18 model."""
-    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
-    return model
+    return ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
 
 
 def resnet34(**kwargs):
     """Constructs a ResNet-34 model."""
-    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
-    return model
+    return ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
 
 
 def resnet50(**kwargs):
     """Constructs a ResNet-50 model."""
-    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-    return model
+    return ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
 
 
 def resnet101(**kwargs):
     """Constructs a ResNet-101 model."""
-    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
-    return model
+    return ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
 
 
 def resnet152(**kwargs):
     """Constructs a ResNet-152 model."""
-    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
-    return model
+    return ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
