Fix gradient issues in finetuning methods

easyfsl/methods/few_shot_classifier.py
@@ -1,8 +1,9 @@
 from abc import abstractmethod
 
+import torch
 from torch import nn, Tensor
 
-from easyfsl.utils import compute_backbone_output_shape
+from easyfsl.utils import compute_backbone_output_shape, compute_prototypes
 
 
 class FewShotClassifier(nn.Module):
@@ -82,3 +83,44 @@ class FewShotClassifier(nn.Module):
             output as it was, or output as soft probabilities
         """
         return output.softmax(-1) if self.use_softmax else output
+
+    def l2_distance_to_prototypes(self, samples: Tensor) -> Tensor:
+        """
+        Compute prediction logits from their euclidean distance to support set prototypes.
+        Args:
+            samples: features of the items to classify
+
+        Returns:
+            prediction logits
+        """
+        return -torch.cdist(samples, self.prototypes)
+
+    def cosine_distance_to_prototypes(self, samples) -> Tensor:
+        """
+        Compute prediction logits from their cosine distance to support set prototypes.
+        Args:
+            samples: features of the items to classify
+
+        Returns:
+            prediction logits
+        """
+        return (
+            nn.functional.normalize(samples, dim=1)
+            @ nn.functional.normalize(self.prototypes, dim=1).T
+        )
+
+    def store_support_set_data(
+        self,
+        support_images: Tensor,
+        support_labels: Tensor,
+    ):
+        """
+        Extract support features, compute prototypes,
+            and store support labels, features, and prototypes
+        Args:
+            support_images: images of the support set
+            support_labels: labels of support set images
+        """
+        self.support_labels = support_labels
+        self.support_features = self.backbone(support_images)
+        self.prototypes = compute_prototypes(self.support_features, support_labels)

easyfsl/methods/finetune.py
@@ -13,6 +13,9 @@ class Finetune(FewShotClassifier):
 
     Fine-tune prototypes based on classification error on support images.
     Classify queries based on their cosine distances to updated prototypes.
+    As is, it is incompatible with episodic training because we freeze the backbone to perform
+    fine-tuning.
+
     This is an inductive method.
     """
 
@@ -29,6 +32,11 @@ class Finetune(FewShotClassifier):
             fine_tuning_lr: learning rate for fine-tuning
         """
         super().__init__(*args, **kwargs)
+
+        # Since we fine-tune the prototypes we need to make them leaf variables
+        # i.e. we need to freeze the backbone.
+        self.backbone.requires_grad_(False)
+
         self.fine_tuning_steps = fine_tuning_steps
         self.fine_tuning_lr = fine_tuning_lr
 
@@ -43,7 +51,7 @@ class Finetune(FewShotClassifier):
             support_images: images of the support set
             support_labels: labels of support set images
         """
-        self.store_features_labels_and_prototypes(support_images, support_labels)
+        self.store_support_set_data(support_images, support_labels)
 
     def forward(
         self,
@@ -60,21 +68,21 @@ class Finetune(FewShotClassifier):
         """
         query_features = self.backbone.forward(query_images)
 
-        # Run adaptation
-        self.prototypes.requires_grad_()
-        optimizer = torch.optim.Adam([self.prototypes], lr=self.fine_tuning_lr)
-        for _ in range(self.fine_tuning_steps):
+        with torch.enable_grad():
+            self.prototypes.requires_grad_()
+            optimizer = torch.optim.Adam([self.prototypes], lr=self.fine_tuning_lr)
+            for _ in range(self.fine_tuning_steps):
 
-            support_logits = self.get_logits_from_cosine_distances_to_prototypes(
-                self.support_features
-            )
-            loss = nn.functional.cross_entropy(support_logits, self.support_labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                support_logits = self.cosine_distance_to_prototypes(
+                    self.support_features
+                )
+                loss = nn.functional.cross_entropy(support_logits, self.support_labels)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
         return self.softmax_if_specified(
-            self.get_logits_from_cosine_distances_to_prototypes(query_features)
+            self.cosine_distance_to_prototypes(query_features)
         ).detach()
 
     @staticmethod

easyfsl/methods/tim.py
@@ -14,6 +14,9 @@ class TIM(FewShotClassifier):
         1) classification error on support images
         2) mutual information between query features and their label predictions
     Classify w.r.t. to euclidean distance to updated prototypes.
+    As is, it is incompatible with episodic training because we freeze the backbone to perform
+    fine-tuning.
+
     TIM is a transductive method.
     """
 
@@ -36,6 +39,11 @@ class TIM(FewShotClassifier):
             conditional_entropy_weight: weight given to the conditional entropy term of the loss
         """
         super().__init__(*args, **kwargs)
+
+        # Since we fine-tune the prototypes we need to make them leaf variables
+        # i.e. we need to freeze the backbone.
+        self.backbone.requires_grad_(False)
+
         self.fine_tuning_steps = fine_tuning_steps
         self.fine_tuning_lr = fine_tuning_lr
         self.cross_entropy_weight = cross_entropy_weight
@@ -53,7 +61,7 @@ class TIM(FewShotClassifier):
             support_images: images of the support set
             support_labels: labels of support set images
         """
-        self.store_features_labels_and_prototypes(support_images, support_labels)
+        self.store_support_set_data(support_images, support_labels)
 
     def forward(
         self,
@@ -74,42 +82,43 @@ class TIM(FewShotClassifier):
         num_classes = self.support_labels.unique().size(0)
         support_labels_one_hot = nn.functional.one_hot(self.support_labels, num_classes)
 
-        self.prototypes.requires_grad_()
-        optimizer = torch.optim.Adam([self.prototypes], lr=self.fine_tuning_lr)
+        with torch.enable_grad():
+            self.prototypes.requires_grad_()
+            optimizer = torch.optim.Adam([self.prototypes], lr=self.fine_tuning_lr)
 
-        for _ in range(self.fine_tuning_steps):
-            support_logits = self.get_logits_from_euclidean_distances_to_prototypes(
-                self.support_features
-            )
-            query_logits = self.get_logits_from_euclidean_distances_to_prototypes(
-                query_features
-            )
+            for _ in range(self.fine_tuning_steps):
+                support_logits = self.l2_distance_to_prototypes(self.support_features)
+                query_logits = self.l2_distance_to_prototypes(query_features)
 
-            support_cross_entropy = (
-                -(support_labels_one_hot * support_logits.log_softmax(1)).sum(1).mean(0)
-            )
+                support_cross_entropy = (
+                    -(support_labels_one_hot * support_logits.log_softmax(1))
+                    .sum(1)
+                    .mean(0)
+                )
 
-            query_soft_probs = query_logits.softmax(1)
-            query_conditional_entropy = (
-                -(query_soft_probs * torch.log(query_soft_probs + 1e-12)).sum(1).mean(0)
-            )
+                query_soft_probs = query_logits.softmax(1)
+                query_conditional_entropy = (
+                    -(query_soft_probs * torch.log(query_soft_probs + 1e-12))
+                    .sum(1)
+                    .mean(0)
+                )
 
-            marginal_prediction = query_soft_probs.mean(0)
-            marginal_entropy = -(
-                marginal_prediction * torch.log(marginal_prediction)
-            ).sum(0)
+                marginal_prediction = query_soft_probs.mean(0)
+                marginal_entropy = -(
+                    marginal_prediction * torch.log(marginal_prediction)
+                ).sum(0)
 
-            loss = self.cross_entropy_weight * support_cross_entropy - (
-                self.marginal_entropy_weight * marginal_entropy
-                - self.conditional_entropy_weight * query_conditional_entropy
-            )
+                loss = self.cross_entropy_weight * support_cross_entropy - (
+                    self.marginal_entropy_weight * marginal_entropy
+                    - self.conditional_entropy_weight * query_conditional_entropy
+                )
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
         return self.softmax_if_specified(
-            self.get_logits_from_euclidean_distances_to_prototypes(query_features)
+            self.l2_distance_to_prototypes(query_features)
         ).detach()
 
     @staticmethod

easyfsl/methods/transductive_finetuning.py
@@ -19,6 +19,9 @@ class TransductiveFinetuning(FewShotClassifier):
     This is a transductive method.
     WARNING: this implementation only updates prototypes, not the whole set of model's
     parameters. Updating the model's parameters raises performance issues that we didn't
+    As is, it is incompatible with episodic training because we freeze the backbone to perform
+    fine-tuning.
+
     have time to solve yet.
     """
 
@@ -35,6 +38,11 @@ class TransductiveFinetuning(FewShotClassifier):
             fine_tuning_lr: learning rate for fine-tuning
         """
         super().__init__(*args, **kwargs)
+
+        # Since we fine-tune the prototypes we need to make them leaf variables
+        # i.e. we need to freeze the backbone.
+        self.backbone.requires_grad_(False)
+
         self.fine_tuning_steps = fine_tuning_steps
         self.fine_tuning_lr = fine_tuning_lr
 
@@ -49,7 +57,7 @@ class TransductiveFinetuning(FewShotClassifier):
             support_images: images of the support set
             support_labels: labels of support set images
         """
-        self.store_features_labels_and_prototypes(support_images, support_labels)
+        self.store_support_set_data(support_images, support_labels)
 
     def forward(
         self,
@@ -66,26 +74,25 @@ class TransductiveFinetuning(FewShotClassifier):
         """
         query_features = self.backbone.forward(query_images)
 
-        self.prototypes.requires_grad_()
-        optimizer = torch.optim.Adam([self.prototypes], lr=self.fine_tuning_lr)
-        for _ in range(self.fine_tuning_steps):
+        with torch.enable_grad():
+            self.prototypes.requires_grad_()
+            optimizer = torch.optim.Adam([self.prototypes], lr=self.fine_tuning_lr)
+            for _ in range(self.fine_tuning_steps):
 
-            support_cross_entropy = nn.functional.cross_entropy(
-                self.get_logits_from_euclidean_distances_to_prototypes(
-                    self.support_features
-                ),
-                self.support_labels,
-            )
-            query_conditional_entropy = entropy(
-                self.get_logits_from_euclidean_distances_to_prototypes(query_features)
-            )
-            loss = support_cross_entropy + query_conditional_entropy
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                support_cross_entropy = nn.functional.cross_entropy(
+                    self.l2_distance_to_prototypes(self.support_features),
+                    self.support_labels,
+                )
+                query_conditional_entropy = entropy(
+                    self.l2_distance_to_prototypes(query_features)
+                )
+                loss = support_cross_entropy + query_conditional_entropy
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
         return self.softmax_if_specified(
-            self.get_logits_from_euclidean_distances_to_prototypes(query_features)
+            self.l2_distance_to_prototypes(query_features)
         ).detach()
 
     @staticmethod