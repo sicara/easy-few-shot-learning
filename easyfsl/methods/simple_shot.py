63af572ec1677f23edf50f3e7dec828279e7a6d1
alanqrwang <alanqrwang@gmail.com>
2023-08-09 14:09:24 -0400

    Added optional centering to SimpleShot

--- easyfsl/methods/simple_shot.py
+++ easyfsl/methods/simple_shot.py
@@ -1,16 +1,41 @@
 from torch import Tensor
 
-from .prototypical_networks import PrototypicalNetworks
+from .few_shot_classifier import FewShotClassifier
+from .utils import compute_prototypes
 
 
-class SimpleShot(PrototypicalNetworks):
+class SimpleShot(FewShotClassifier):
     """
     Yan Wang, Wei-Lun Chao, Kilian Q. Weinberger, and Laurens van der Maaten.
     "SimpleShot: Revisiting Nearest-Neighbor Classification for Few-Shot Learning" (2019)
     https://arxiv.org/abs/1911.04623
 
-    Almost exactly Prototypical Classification, but with cosine distance instead of euclidean distance.
+    Almost exactly Prototypical Classification, but with (optional) centering and cosine distance instead of euclidean distance.
     """
+    def __init__(self, 
+                 *args, 
+                 feature_mean: Tensor = None,
+                 **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.feature_mean = feature_mean
+
+    def process_support_set(
+        self,
+        support_images: Tensor,
+        support_labels: Tensor,
+    ):
+        """
+        Overrides process_support_set of FewShotClassifier.
+        Extract feature vectors from the support set and store class prototypes.
+        """
+
+        support_features = self.backbone.forward(support_images)
+        self._raise_error_if_features_are_multi_dimensional(support_features)
+        if self.feature_mean is not None:
+            support_features = support_features - self.feature_mean
+
+        self.prototypes = compute_prototypes(support_features, support_labels)
 
     def forward(
         self,
@@ -25,6 +50,8 @@ class SimpleShot(PrototypicalNetworks):
         """
         query_features = self.backbone(query_images)
         self._raise_error_if_features_are_multi_dimensional(query_features)
+        if self.feature_mean is not None:
+            query_features = query_features - self.feature_mean
 
         scores = self.cosine_distance_to_prototypes(query_features)
 
--- easyfsl/utils.py
+++ easyfsl/utils.py
@@ -160,3 +160,26 @@ def evaluate(
                 tqdm_eval.set_postfix(accuracy=correct_predictions / total_predictions)
 
     return correct_predictions / total_predictions
+
+def compute_feature_mean(
+    dataloader: DataLoader, 
+    model: nn.Module, 
+    device: str = "cuda"):
+    """
+    Compute the mean of the support features.
+    Assumes the images are always first element of the batch.
+    Returns:
+        Tensor: shape (1, feature_dimension)
+    """
+    model.to(device)
+    model.eval()
+    all_embeddings = []
+    with torch.no_grad():
+        for batch in tqdm(
+            dataloader, unit="batch", desc="Compute feature mean"
+        ):
+            images = batch[0].to(device)
+            all_embeddings.append(model(images).detach().cpu())
+
+    concatenated_embeddings = torch.cat(all_embeddings)
+    return concatenated_embeddings.mean(dim=0).to(device)
